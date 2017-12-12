be39d4eb4b2d959962ea4d43bb3c0e1f1ef55987
gottsch <mark.gottschling@gmail.com>
2017-12-11 20:06:10 -0500

    Update files to reflect version 1.0.8.

--- FastLadder1.12.2/src/com/someguyssoftware/fastladder/FastLadder.java
+++ FastLadder1.12.2/src/com/someguyssoftware/fastladder/FastLadder.java
@@ -45,7 +45,7 @@ public class FastLadder extends AbstractMod {
 	// constants
 	public static final String MODID = "fastladder";
 	public static final String NAME = "FastLadder!";
-	public static final String VERSION = "1.0.7";
+	public static final String VERSION = "1.0.8";
 	public static final String UPDATE_JSON_URL = "https://raw.githubusercontent.com/gottsch/gottsch-minecraft-FastLadder/master/FastLadder1.12/update.json";
 	
 	// TODO create BuilderVersion that parses the minecraft forge updatejson file instead of custom format file
