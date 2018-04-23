ae5f3c2c4f065ac8ba02dd921d8b8113961e8145
gottsch <17928819+gottsch@users.noreply.github.com>
2018-04-23 14:40:46 -0400

    Updates

--- FastLadder1.12/src/com/someguyssoftware/fastladder/FastLadder.java
+++ FastLadder1.12/src/com/someguyssoftware/fastladder/FastLadder.java
@@ -47,7 +47,7 @@ public class FastLadder extends AbstractMod {
 	// constants
 	public static final String MODID = "fastladder";
 	public static final String NAME = "FastLadder!";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 
 	public static final String UPDATE_JSON_URL = "https://raw.githubusercontent.com/gottsch/gottsch-minecraft-FastLadder/master/FastLadder1.12/update.json";
 	
